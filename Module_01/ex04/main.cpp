4c578338b72989e21994000dbf60d07e45e5b155
lter-zak <terzaqaryan@inbox.ru>
2023-06-27 22:08:36 +0400

    af

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -5,48 +5,37 @@ int main(int nb, char **str)
 {
 	(void)str;
 	(void)nb;
-	//if ( nb == 4)
-	//{
-	// 	std::string		fileName;
-	// 	std::string		S1;
-	// 	std::string		S2;
-	// 	//char c;
-	// 	std::ofstream	MyFile;
-	// 	std::ifstream	oldFile;
+	if ( nb == 4)
+	{
+		std::string		S1;
+		std::string		S2;
+		std::ifstream myFile;
+		std::string fileStr;
+		std::string nweFileName;
+		std::ifstream oldFile;
 
-	// 	fileName = str[1];
-	// 	S1		 = str[2];
-	// 	S2		 = str[3];
-	// 	oldFile.open("oldFile");
-	// 	if(!oldFile) 
-	// 	{ // file couldn't be opened
-    //   		std::cerr << "Error: file could not be opened" <<std::endl;
-	// 		return(0);
-	// 	}
-	// 	oldFile.is_open();
-	// 	std::getline (oldFile, S1);
-	// 	// oldFile>>S1;
-	// 	std::cout << S1 << ": " << oldFile.tellg() << '\n';
-	// 	// c =	oldFile.get();
-	// 	// std::cout<<c;
-	// 	//std::cout<<c<<std::endl;
-	// 	// std::ofstream MyFile(fileName + ".replace");
-	// 	//MyFile << "Files can be tricky, but it is fun enough!";
-	// //}
+		S1		 = str[2];
+		S2		 = str[3];
 
-	std::string output;
-    std::ifstream myfile;
-    std::ofstream myfile2;
-
-   std::string STRING;
-    myfile.open ("oldFile");
-
-    //myfile2 << "Lucas, It Worked";
-
-        myfile >> STRING;
-        std::cout << STRING << std::endl;
-		 myfile >> STRING;
-        std::cout << STRING << std::endl;
-    myfile.close();
-	return (0);
+		nweFileName = str[1];
+		std::ofstream newFile(nweFileName + ".replace");
+		oldFile.open ("test.txt");
+		if(!oldFile) 
+		{
+			std::cerr << "Error: file could not be opened" <<std::endl;
+			return(0);
+		}
+		else
+		{
+			while ( getline(oldFile, fileStr))
+			{
+				std::cout<<fileStr<<std::endl;
+			}
+		}
+	}
+	else
+	{
+		std::cerr << "not enaugh arguments" <<std::endl;
+	}
+		return (0);
 }
