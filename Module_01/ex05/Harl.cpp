7d130aeff9775a4ecb2ecfc298c038c5efef8d1d
lter-zak <terzaqaryan@inbox.ru>
2023-07-04 13:48:01 +0400

    eee

--- Module_01/ex05/Harl.cpp
+++ Module_01/ex05/Harl.cpp
@@ -2,6 +2,38 @@
 
 void Harl::debug()
 {
-    std::cout<<"I love having extra bacon for my 7XL-double-cheese-triple
-                -pickle-special- ketchup burger. I really do!"
+    std::cout<<"I love having extra bacon for my 7XL-double-cheese-triple\n
+                -pickle-special- ketchup burger. I really do!"<<std::endl;
 }
+
+void Harl::info()
+{
+    std::cout<<"I cannot believe adding extra bacon costs more money.\n 
+            You didn’t put enough bacon in my burger! If you did, I wouldn’t\n
+             be asking for more!"<<std::endl;
+
+}
+
+void Harl::warning()
+{
+    std::cout<<"I think I deserve to have some extra bacon for free.\n
+                 I’ve been coming for years whereas you started working\n 
+                 here since last month."<<std::endl;
+}
+
+void Harl::error()
+{
+    std::cout<<"This is unacceptable! I want to speak to the manager now."<<std::endl;
+}
+
+
+void Harl::complain(std::string level)
+{
+    //return_type (class_name::*pointer_name) (parameter types)
+    void        (Harl::*memberFuncPtr[4]) () = {};
+    std::string chLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"}
+    int         i = 0;
+
+    for ( , i < 4, chLevel[i].co)
+}
+    
